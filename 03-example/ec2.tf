cefbaeda30b481d55ef77094ee8f572bd8a07667
Snehil Shubham <snehil1693@gmail.com>
2025-01-16 16:57:02 +0530

    making changes for terraform

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,12 +1,12 @@
-# resource "aws_instance" "sample" {
-#   ami                 = "ami-0b4f379183e5706b9"
-#   instance_type       = "t2.micro"
-#   vpc_security_group_ids = []
-#
-#   tags                = {
-#     Name              = "Sample"
-#   }
-# }
+resource "aws_instance" "sample" {
+  ami                 = "ami-0b4f379183e5706b9"
+  instance_type       = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags                = {
+    Name              = "Sample"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
@@ -33,8 +33,8 @@ resource "aws_security_group" "allow_ssh" {
 
 }
 
-output "sg-attributes" {
-  value = aws_security_group.allow_ssh
+output "ec2-attributes" {
+  value = aws_instance.sample
 }
 
 provider "aws" {
