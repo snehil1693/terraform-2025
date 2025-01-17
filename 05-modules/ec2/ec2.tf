1f9b365c4dc99f7bada944aa45b10f0f2bee180e
Snehil Shubham <snehil1693@gmail.com>
2025-01-17 18:19:36 +0530

    making changes for terraform

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,12 +1,18 @@
 resource "aws_instance" "sample" {
+  count                  =  2
   ami                    = "ami-0b4f379183e5706b9"
-  instance_type          = "t2.micro"
+  instance_type          = var.TYPE
 #   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   vpc_security_group_ids = [var.SG_ID]
 
   tags                   = {
-    Name                 = "Sample"
+    Name                 = "Example-${count.index}"
   }
 }
 
 variable "SG_ID" {}
+variable "TYPE" {}
+
+output "PRIVATE_IP" {
+  value = aws_instance.sample.*.private_ip
+}
