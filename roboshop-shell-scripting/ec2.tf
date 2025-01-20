fcf21c09467dad99d371ac909fcee0bb99172684
Snehil Shubham <snehil1693@gmail.com>
2025-01-20 13:43:21 +0530

    making changes for terraform

---
+++ roboshop-shell-scripting/ec2.tf
@@ -0,0 +1,17 @@
+# resource "aws_instance" "instances" {
+#   ami                = "ami-0b4f379183e5706b9"
+#   instance_type      = "t2.micro"
+# }
+
+resource "aws_spot_instance_request" "instances" {
+  count                = length(var.COMPONENTS)
+  ami                  = "ami-0b4f379183e5706b9"
+  spot_price           = "0.0037"
+  instance_type        = "t2.micro"
+  wait_for_fulfillment = true
+
+
+  tags                 = {
+    Name               = element(var.COMPONENTS, count.index)
+  }
+}
