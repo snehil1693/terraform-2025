ace16919fdf7ebe5005761b91697dc64492566f2
Snehil Shubham <snehil1693@gmail.com>
2025-01-21 15:33:32 +0530

    making changes for terraform

--- 07-data-sources/data.tf
+++ 07-data-sources/data.tf
@@ -1,9 +1,29 @@
 
 data "aws_ami" "example" {
-  most_recent = true
-  owners = [973714476881]
+  most_recent       = true
+  name_regex        = "Centos*"
+  owners            = [973714476881]
+}
+
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  filter {
+    name            = "product-description"
+    values          = ["Linux/UNIX"]
+  }
+
 }
 
 output "amis" {
-  value = data.aws_ami.example
+  value            = data.aws_ami.example.id
+}
+
+output "spot" {
+  value            = data.aws_ec2_spot_price.example
+}
+
+provider "aws" {
+  region          = "us-east-1"
 }
